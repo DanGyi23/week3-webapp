1d5df65154888c68e342e16405aaf1c99b4f0504
Dan Gyi <daniel.gyi@gmail.com>
2019-09-02 15:56:29 +0100

    Britneys border has no whitespace

--- app.rb
+++ app.rb
@@ -13,9 +13,5 @@ get '/dan/smells' do
 end
 
 get '/cat' do
-  "<div>MEOW!!!</div>
-  <div style='border:3px dashed red;border_radius:12px;margin:12rem'>
-  <img src='https://www.rollingstone.com/wp-content/uploads/2019/05/britney-spears.jpg?resize=900,600&w=440'>
-  </div>"
-
+  erb(:index)
 end
